c36aa38e8fe37b7169aa83748549d8929da5d0e8
Emma Paris <eparis@atlassian.com>
2020-05-19 01:13:30 -0500

    Commit

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,9 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField, TextAreaField, PasswordField
+from wtforms.validators import DataRequired, InputRequired, Email
 
+
+class UploadForm(FlaskForm):
+    description = TextAreaField('Description', validators=[DataRequired()])
+    photo = FileField('Profile Photo', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'Images only!'])])
